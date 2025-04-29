2bb7f7fc53fc68a635d4bb1e87097bcad2bb9574
Arthur <arthurbarbo177@gmail.com>
2025-04-29 20:24:52 +0100

    correção do revisor

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,30 @@
+const form = document.querySelector(".popup__profile");
 const h1 = document.querySelector("h1");
-const editname = document.querySelector("#name");
+const editname = form.querySelector("#name");
 const h2 = document.querySelector(".profile__description");
-const sub = document.querySelector("#For_me");
+const sub = form.querySelector("#For_me");
 
 const editprofile = document.querySelector(".profile__pen");
-const editsave = document.querySelector("#save");
+
 const closepopup = document.querySelector("#close");
 const popup = document.querySelector(".popup");
 
-editsave.addEventListener("click", function () {
+/*botao de save*/ form.addEventListener("submit", function (event) {
+  event.preventDefault();
   h1.textContent = editname.value;
   h2.textContent = sub.value;
   popup.classList.add("popup-hidden");
   popup.classList.remove("popup");
 });
 
-editprofile.addEventListener("click", function () {
+/* canetinha*/ editprofile.addEventListener("click", function () {
   editname.value = h1.textContent;
   sub.value = h2.textContent;
   popup.classList.remove("popup-hidden");
   popup.classList.add("popup");
 });
 
-closepopup.addEventListener("click", function () {
+/*close*/ closepopup.addEventListener("click", function () {
   popup.classList.add("popup-hidden");
   popup.classList.remove("popup");
 });
