47db22b230bb5f5445325faf5c40200a2aa753f4
Arthur <arthurbarbo177@gmail.com>
2025-05-01 00:01:45 +0100

    Hotfix2

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,24 @@ const form = document.querySelector(".popup__profile");
 const title = document.querySelector("h1");
 const editname = form.querySelector("#name");
 const description = document.querySelector(".profile__description");
-const sub = form.querySelector("#For_me");
 
+
+const about = form.querySelector("#about");
 const editprofile = document.querySelector(".profile__pen");
-
 const closepopup = document.querySelector("#close");
 const popup = document.querySelector(".popup");
 
 /*botao de save*/ form.addEventListener("submit", function (event) {
   event.preventDefault();
   title.textContent = editname.value;
-  description.textContent = sub.value;
+  description.textContent = about.value;
   popup.classList.add("popup-hidden");
   popup.classList.remove("popup");
 });
 
 /* canetinha*/ editprofile.addEventListener("click", function () {
   editname.value = title.textContent;
-  sub.value = description.textContent;
+  about.value = description.textContent;
   popup.classList.remove("popup-hidden");
   popup.classList.add("popup");
 });
