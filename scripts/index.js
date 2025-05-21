45e949cf203df232843e1597f5aed4690947a4a3
Arthur <arthurbarbo177@gmail.com>
2025-05-21 22:07:52 +0100

    Hotfix-1= correção de roteiro

--- scripts/index.js
+++ scripts/index.js
@@ -117,5 +117,36 @@ function createCard(imageSrc, titleText) {
     zoompopup.classList.remove("popup-hidden");
   });
 
-  document.querySelector(".elements").appendChild(clone);
+  document.querySelector(".elements").prepend(clone);
 }
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  }
+];
+
+initialCards.forEach((card) => {
+  createCard(card.link, card.name);
+});
