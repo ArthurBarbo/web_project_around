c217771703a52d75a68a5f30754c360974810082
Arthur <arthurbarbo177@gmail.com>
2025-07-14 22:32:38 +0100

    set up server and functions

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ import { UserInfo } from "./UserInfo.js";
 import { PopupWithForm } from "./PopupWithForm.js";
 import { PopupWithImage } from "./PopupWithImage.js";
 import { Section } from "./Section.js";
-import { Card, initialCards } from "./Card.js";
+import { Card } from "./Card.js";
 import { FormValidator } from "./formValidator.js";
 import { Utils } from "./utils.js";
 
@@ -21,23 +21,35 @@ const utils = new Utils({
   zoomCaption: document.querySelector("#popupCaption"),
   zoompopup: document.querySelector("#popup"),
 });
+const userInfo = new UserInfo({
+  nameSelector: ".profile__name",
+  aboutSelector: ".profile__description",
+});
+
+const token = "79b24936-a205-4985-b1d0-6eeb1d8f406b";
+const apiConfig = {
+  baseUrl: "https://around-api.pt-br.tripleten-services.com/v1",
+  headers: {
+    authorization: token,
+    "Content-Type": "application/json",
+  },
+};
 
 const popupWithImage = new PopupWithImage(
   "#popup",
   document.querySelector("#popupimg"),
   document.querySelector("#popupCaption")
 );
+popupWithImage.setEventListeners();
 
 function handleCardClick(name, link) {
   console.log("handleCardClick chamada com:", name, link);
   popupWithImage.open({ name, link });
 }
 
-let cardSection;
-
-cardSection = new Section(
+const cardSection = new Section(
   {
-    items: initialCards,
+    items: [],
     renderer: (data) => {
       const card = new Card(
         data.name,
@@ -50,17 +62,61 @@ cardSection = new Section(
   },
   ".elements"
 );
+fetch(`${apiConfig.baseUrl}/users/me`, {
+  headers: apiConfig.headers,
+})
+  .then((res) => res.json())
+  .then((userData) => {
+    userInfo.setUserInfo({
+      name: userData.name,
+      about: userData.about,
+    });
+  })
+  .catch((err) => {
+    console.log("Erro ao buscar dados do usuário:", err);
+  });
 
-const userInfo = new UserInfo({
-  nameSelector: ".profile__name",
-  aboutSelector: ".profile__description",
-});
+fetch(`${apiConfig.baseUrl}/cards`, {
+  headers: apiConfig.headers,
+})
+  .then((res) => res.json())
+  .then((cardServer) => {
+    cardSection._items = cardServer;
+    cardSection.renderItems();
+  })
+  .catch((err) => {
+    console.log("Erro ao buscar cartões:", err);
+  });
 
 const profilePopup = new PopupWithForm("#popup-profile", (formData) => {
-  userInfo.setUserInfo({
-    name: formData.name,
-    about: formData.about,
-  });
+  fetch(`${apiConfig.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: {authorization: token,
+      "Content-Type":"application/json",
+  },
+    body: JSON.stringify({
+      name: formData.name,
+      about: formData.about,
+    }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        console.log("Erro ao atualizar perfil. Código de status:", res.status);
+      }
+    })
+    .then((updatedUser) => {
+      if (updatedUser) {
+        userInfo.setUserInfo({
+          name: updatedUser.name,
+          about: updatedUser.about,
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("Erro na requisição:", err);
+    });
 });
 
 profilePopup.setEventListeners();
--- scripts/utils.js
+++ scripts/utils.js
@@ -9,10 +9,6 @@ export class Utils {
     editprofile,
     addpic,
     photoAdd,
-    imageCards,
-    zoomImg,
-    zoomCaption,
-    zoompopup,
   }) {
     this._form = form;
     this._title = title;
@@ -23,10 +19,6 @@ export class Utils {
     this._editprofile = editprofile;
     this._addpic = addpic;
     this._photoAdd = photoAdd;
-    this._imageCards = imageCards;
-    this._zoomImg = zoomImg;
-    this._zoomCaption = zoomCaption;
-    this._zoompopup = zoompopup;
 
     this.setEventListeners();
   }
@@ -46,38 +38,6 @@ export class Utils {
       this._popup.classList.add("popup");
     });
 
-    document
-      .querySelectorAll(".popup__close,.popup__close-zoom")
-      .forEach((button) => {
-        button.addEventListener("click", () => {
-          const popup = button.closest(".popup");
-          if (popup) {
-            popup.classList.add("popup-hidden");
-            const form = popup.querySelector("form");
-            if (form) {
-              form.reset();
-              const inputs = form.querySelectorAll("input");
-              inputs.forEach((input) => {
-                input.classList.remove("popup__input_type_error");
-                const errorSpan = form.querySelector(`#${input.id}-error`);
-                if (errorSpan) {
-                  errorSpan.textContent = "";
-                  errorSpan.classList.remove("popup__error_visible");
-                }
-              });
-            }
-          }
-        });
-      });
-
-    document.querySelectorAll(".popup").forEach((popup) => {
-      popup.addEventListener("click", (event) => {
-        if (event.target === event.currentTarget) {
-          popup.classList.add("popup-hidden");
-        }
-      });
-    });
-
     document.addEventListener("keydown", (evt) => {
       if (evt.key === "Escape") {
         document.querySelectorAll(".popup").forEach((popup) => {
@@ -90,32 +50,6 @@ export class Utils {
 
     this._addpic.addEventListener("click", () => {
       this._photoAdd.classList.remove("popup-hidden");
-    });
-
-    document.querySelectorAll(".elements__remove").forEach((button) => {
-      button.addEventListener("click", () => {
-        const card = button.closest(".elements__card");
-        if (card) card.remove();
-      });
-    });
-
-    this._imageCards.forEach((img) => {
-      img.addEventListener("click", () => {
-        this._zoomImg.src = img.src;
-        this._zoomImg.alt = img.alt;
-
-        const caption = img
-          .closest(".elements__card")
-          .querySelector(".elements__text");
-        this._zoomCaption.textContent = caption ? caption.textContent : "";
-        this._zoompopup.classList.remove("popup-hidden");
-      });
-    });
-
-    document.querySelectorAll(".elements__like").forEach((button) => {
-      button.addEventListener("click", () => {
-        button.classList.toggle("elements__like-on");
-      });
     });
   }
 }
